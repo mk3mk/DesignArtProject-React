8c886efb02fac93aec149cc14a7facabead20923
mk3mk <mk3mk@yandex.ru>
2024-02-16 16:15:23 +0300

    1602

--- src/pages/Panel2.jsx
+++ src/pages/Panel2.jsx
@@ -4,8 +4,8 @@ const Panel2 = () => {
         <h1>Panel2</h1>
           <div className="tab-content" id="nav-tabContent">
             <div className="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-              <p className="text-center mt-2"><img src="https://api.designartproject.ru/images/3b536b.gif" alt=""/></p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus maxime aut ea impedit voluptates aperiam dolor laborum officiis autem aspernatur! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, alias, repudiandae sunt illo consequatur aperiam doloribus nesciunt ut deserunt ipsa est tempora nihil. Totam eveniet aperiam debitis fugit ipsa doloremque. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio sequi dolorem suscipit assumenda molestiae voluptatem qui consequuntur magni? Deleniti, corporis.</p>
+              <p className="text-center mt-2"><img src="https://api.designartproject.ru/images/300х100-02.gif" alt=""/></p>
+              <p>Loicing elit. Minus maxime aut ea impedit voluptates aperiam dolor laborum officiis autem aspernatur! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, alias, repudiandae sunt illo conseq......</p>
             </div>
           </div>
         </div>
