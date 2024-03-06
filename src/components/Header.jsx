b4b6cb63a0953f95f4ea17907288fc8d1f5c028b
mk3mk <mk3mk@yandex.ru>
2024-03-06 10:49:37 +0300

    0603

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimney, faSailboat, faClipboardList, faBox, faShieldHalved } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
@@ -16,16 +18,18 @@ const Header = () => {
        <div className="border-bottom lh-1 py-3">
          <div className="row flex-nowrap justify-content-between align-items-center">
            <div className="col-4 pt-1">
-             <a className="link-secondary" href="/">Subscribe</a>
+             <Link className="link-secondary" to="/">Subscribe</Link>
            </div>
            <div className="col-4 text-center">
-             <a className="blog-header-logo blog-div-logo text-body-emphasis text-decoration-none" href="/">Large</a>
+             <Link className="blog-header-logo blog-div-logo text-body-emphasis text-decoration-none" to="/">
+              DesignArtProject
+             </Link>
            </div>
            <div className="col-4 d-flex justify-content-end align-items-center">
              <a className="link-secondary" href="/" aria-label="Search">
                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
              </a>
-             <a className="btn btn-sm btn-outline-secondary" href="/login">Sign up</a>
+             <Link className="btn btn-sm btn-outline-secondary" to="/login">Sign up</Link>
            </div>
          </div>
        </div>
@@ -75,11 +79,26 @@ const Header = () => {
                   <Nav.Link href="/storepage">Store</Nav.Link>
                   <Nav.Link href="/contacts">Contacts</Nav.Link> */}
 
-           <Link className="nav-item nav-link link-body-emphasis active" to="/">Home</Link>
-           <Link className="nav-item nav-link link-body-emphasis" to="/posts">Blog</Link>
-           <Link className="nav-item nav-link link-body-emphasis" to="/about">About</Link>
-           <Link className="nav-item nav-link link-body-emphasis" to="/storepage">Store</Link>
-           <Link className="nav-item nav-link link-body-emphasis" to="/contacts">Contacts</Link>
+           <Link className="nav-item nav-link link-body-emphasis" to="/">
+           <FontAwesomeIcon icon={faHouseChimney} className="mr-2" />
+            Home
+            </Link>
+           <Link className="nav-item nav-link link-body-emphasis" to="/posts">
+           <FontAwesomeIcon icon={faSailboat} className="mr-2" />
+            Blog
+            </Link>
+           <Link className="nav-item nav-link link-body-emphasis" to="/about">
+           <FontAwesomeIcon icon={faClipboardList} className="mr-2" />
+            About
+            </Link>
+           <Link className="nav-item nav-link link-body-emphasis" to="/storepage">
+           <FontAwesomeIcon icon={faBox} className="mr-2" />
+            Store
+            </Link>
+           <Link className="nav-item nav-link link-body-emphasis" to="/contacts">
+           <FontAwesomeIcon icon={faShieldHalved} className="mr-2" />
+            Contacts
+            </Link>
 
                 </Nav>
               </Offcanvas.Body>
